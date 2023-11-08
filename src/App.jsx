b6d747284f5cb87a1b7f370ed7d5e4842026bb61
pavophilip <pavophilip@gmail.com>
2023-11-08 13:32:41 +0100

    update layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,118 +1,104 @@
-import "./App.css";
 import Widget from "remoteApp/Widget";
+
+import classes from './App.module.css';
 
 function App() {
   return (
-    <>
-      <div id="head">
-        <div
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            backgroundColor: "rgb(235, 225, 0)",
-            width: "100vw",
-            height: "40px",
-          }}
-        >
-          <img
-            style={{ position: "absolute", top: "5px", left: "30px" }}
-            src="/pet-shop-demo/assets/head1.png"
-            height="30px"
-          />
-          <img
-            style={{ position: "absolute", top: "5px", right: "30px" }}
-            src="/pet-shop-demo/assets/head2.png"
-            height="30px"
-          />
-        </div>
-        <div
-          style={{
-            position: "absolute",
-            top: "40px",
-            left: 0,
-            backgroundColor: "rgb(254, 242, 0)",
-            width: "100vw",
-            height: "120px",
-          }}
-        >
-          <img
-            style={{ position: "absolute", top: "15px", left: "30px" }}
-            src="/pet-shop-demo/assets/logo.svg"
-            height="86px"
-          />
-          <img
-            style={{ position: "absolute", top: "35px", left: "420px" }}
-            src="/pet-shop-demo/assets/search.png"
-            height="57px"
-          />
-          <img
-            style={{ position: "absolute", top: "35px", right: "30px" }}
-            src="/pet-shop-demo/assets/logo2.png"
-            height="57px"
-          />
-        </div>
-      </div>
+    <div className={classes.page}>
+        <div className={classes.header}>
+            <div className={classes.navbar}>
 
-      <div id="content" style={{ padding: "200px 0 180px 0 !important" }}>
-        <center>
-          <Widget propositionId={4}/>
-        </center>
-      </div>
+            </div>
 
-      <div id="foot">
-        <div
-          style={{
-            position: "absolute",
-            bottom: "160px",
-            left: 0,
-            width: "100vw",
-            height: "80px",
-          }}
-        >
-          <center>
-            <img src="/pet-shop-demo/assets/animal1.png" height="80px" />
-          </center>
-        </div>
-        <div
-          style={{
-            position: "absolute",
-            bottom: "78px",
-            left: 0,
-            backgroundColor: "rgb(0, 158, 224)",
-            width: "100vw",
-            height: "83px",
-          }}
-        >
-          <center>
-            <img src="/pet-shop-demo/assets/animal2.png" height="83px" />
-          </center>
+            <div className={classes.headerContent}>
+                <div className={classes.logo}>
+                    <img src={"https://www.qualipet.ch/static/version1699277464/frontend/Qualipet/default/de_CH/images/logo/qp-de.svg"}/>
+                </div>
+            </div>
+
+            <div className={classes.headerMenu}>
+
+            </div>
         </div>
 
-        <div
-          style={{
-            position: "absolute",
-            bottom: 0,
-            left: 0,
-            backgroundColor: "rgba(254, 242, 0, 0.15)",
-            borderTop: "1px solid #ccc",
-            width: "100vw",
-            height: "40px",
-          }}
-        >
-          <img
-            style={{ position: "absolute", bottom: "5px", left: "30px" }}
-            src="/pet-shop-demo/assets/footer1.png"
-            height="30px"
-          />
-          <img
-            style={{ position: "absolute", bottom: "5px", right: "30px" }}
-            src="/pet-shop-demo/assets/footer2.png"
-            height="30px"
-          />
+        <div className={classes.pageContent}>
+            <div className={classes.placeholderContainer} style={{
+                width: 460
+            }}>
+                <div className={classes.pagePlaceholder} style={{
+                    width: '60%',
+                    backgroundColor: 'rgba(255,237,39,0.51)'
+                }}/>
+
+                <div className={classes.pagePlaceholder} style={{
+                    height: 160,
+                    backgroundColor: 'rgba(255,237,39,0.29)'
+                }}/>
+                <div className={classes.pagePlaceholder} style={{
+                    width: '32%',
+                    backgroundColor: 'rgba(255,237,39,0.51)'
+                }}/>
+
+                <div style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    gap: 8
+                }}>
+                    <div className={classes.pagePlaceholder} style={{
+                        backgroundColor: 'rgba(255,237,39,0.29)',
+                        height: 160,
+                    }}/>
+                    <div className={classes.pagePlaceholder} style={{
+                        backgroundColor: 'rgba(255,237,39,0.29)',
+                        height: 160,
+                    }}/>
+                    <div className={classes.pagePlaceholder} style={{
+                        backgroundColor: 'rgba(255,237,39,0.29)',
+                        height: 160,
+                    }}/>
+                </div>
+
+            </div>
+            <div className={classes.widgetContainer}>
+                <Widget propositionId={52}/>
+            </div>
+
+
+            <div className={classes.placeholderContainer} style={{
+                width: 200
+            }}>
+                <div className={classes.pagePlaceholder} style={{
+                    width: '80%',
+                    backgroundColor: 'rgba(255,237,39,0.51)'
+                }}/>
+
+                <div className={classes.pagePlaceholder} style={{
+                   height: 180,
+                    backgroundColor: 'rgba(255,237,39,0.29)'
+                }}/>
+
+                <div className={classes.pagePlaceholder} style={{
+                    height: 180,
+                    backgroundColor: 'rgba(255,237,39,0.29)'
+                }}/>
+
+                <div className={classes.pagePlaceholder} style={{
+                    height: 180,
+                    backgroundColor: 'rgba(255,237,39,0.29)'
+                }}/>
+
+                <div className={classes.pagePlaceholder} style={{
+                    height: 180,
+                    backgroundColor: 'rgba(255,237,39,0.29)'
+                }}/>
+            </div>
         </div>
-      </div>
-    </>
+
+        <div className={classes.footer}>
+
+        </div>
+
+    </div>
   );
 }
 
